152d5088dd13b4938406c09ca92ff3fac5935e5b
Ryan Harris <ragobash@gmail.com>
2019-09-23 19:48:09 -0500

    Getting functionality set up

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Ryan');
+INSERT INTO burgers (burger_name) VALUES ('Andrew');
+INSERT INTO burgers (burger_name) VALUES ('Josh');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Buddy', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Collin', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Matt', true);
